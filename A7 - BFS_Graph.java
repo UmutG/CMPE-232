7f0d1c6fb26af9fab911e605dd63aa07710e5af5
Umut Geyik <umut.geyik@hotmail.com>
2022-07-30 00:14:46 +0300

    Update A7 - BFS_Graph.java

--- A7 - BFS_Graph.java
+++ A7 - BFS_Graph.java
@@ -2,48 +2,64 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class BFS_Graph {
-	int vertex;
-	
     class Node{
-    	String color;
-    	int d, precessor;
-    	Node next;
+        String color;
+        int d, precessor;
+        Node next;
     }
-    
-    LinkedList <BFS_Graph.Node> Adj[];
-    
+
+    Node node;
+    LinkedList <Node> Adj[];
+
+    void addEdge(int v, Node n){
+        Adj[v].add(n);
+    }
+
     void BFS(BFS_Graph G, Node s) {
-    	for (LinkedList<BFS_Graph.Node> u: G.Adj) {
-    		if (!s.equals(u))
-    		u.color = "WHITE";
-    		u.d = -1;
-    		u.precessor = (Integer) null;
-    	}
-    	s.color = "GRAY";
-    	s.d = 0;
-    	s.precessor = (Integer) null;
-    	
-    	Queue <BFS_Graph.Node> queue = null;
-    	queue.add(s);
-    	
-    	while (!queue.isEmpty()) {
-    		BFS_Graph.Node u = queue.poll();
-    		for (Node v: G.Adj[u]) {
-    			
-    		}
-    	}
+        int i = 0;
+        for (Node u: G.Adj[i]) {
+            if (u != s){
+                u.color = "WHITE";
+                u.d = -1;
+                u.precessor = (Integer) null;
+            }
+            i++;
+        }
+        s.color = "GRAY";
+        s.d = 0;
+        s.precessor = (Integer) null;
+
+        Queue <Node> queue = null;
+        queue.add(s);
+
+        while (!queue.isEmpty()) {
+            Node u = queue.poll();
+            int nu = u.next.precessor;
+
+            for (Node v: G.Adj[nu]){
+                if(v.color == "WHITE"){
+                    v.color = "GRAY";
+                    v.d = u.d + 1;
+                    v.precessor = nu;
+                    queue.add(v);
+                }
+            }
+            u.color = "BLACK";
+        }
     }
 
     public static void main(String[] args) {
-    	BFS_Graph bfsg = new BFS_Graph(5);
-    	bfsg.addEdge(0, 1);
-    	bfsg.addEdge(2, 1);
-    	bfsg.addEdge(3, 1);
-    	bfsg.addEdge(1, 2);
-    	bfsg.addEdge(1, 4);
-    	bfsg.addEdge(4, 3);
-    	bfsg.addEdge(3, 2);
-    	bfsg.addEdge(2, 3);
-    	bfsg.printGraph(bfsg);
+        BFS_Graph bfsg = new BFS_Graph();
+        Node [] nodes = new Node[5];
+
+        bfsg.addEdge(0, nodes[1]);
+        bfsg.addEdge(2, nodes[1]);
+        bfsg.addEdge(3, nodes[1]);
+        bfsg.addEdge(1, nodes[2]);
+        bfsg.addEdge(1, nodes[4]);
+        bfsg.addEdge(4, nodes[3]);
+        bfsg.addEdge(3, nodes[2]);
+        bfsg.addEdge(2, nodes[3]);
+
     }
 }
